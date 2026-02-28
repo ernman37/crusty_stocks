fe253147a43c01c89aecc030f7394aa62731b3ab
ernman37 <Rion.duckworth@gmail.com>
2026-02-28 07:41:55 -0700

    add json feature

--- src/timeframe.rs
+++ src/timeframe.rs
@@ -1,6 +1,5 @@
 use std::fmt;
 
-#[cfg_attr(feature = "json", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, PartialEq)]
 pub enum TimeFrame {
     OneMinute,
@@ -30,10 +29,45 @@ impl TimeFrame {
             TimeFrame::OneMonth => "1M",
         }
     }
+
+    #[cfg(feature = "json")]
+    fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "1m" => Some(TimeFrame::OneMinute),
+            "3m" => Some(TimeFrame::ThreeMinutes),
+            "5m" => Some(TimeFrame::FiveMinutes),
+            "15m" => Some(TimeFrame::FifteenMinutes),
+            "30m" => Some(TimeFrame::ThirtyMinutes),
+            "1h" => Some(TimeFrame::OneHour),
+            "4h" => Some(TimeFrame::FourHours),
+            "1d" => Some(TimeFrame::OneDay),
+            "1w" => Some(TimeFrame::OneWeek),
+            "1M" => Some(TimeFrame::OneMonth),
+            _ => None,
+        }
+    }
 }
 
 impl fmt::Display for TimeFrame {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.as_str())
+    }
+}
+
+#[cfg(feature = "json")]
+impl serde::Serialize for TimeFrame {
+    fn serialize<S: serde::Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
+        s.serialize_str(self.as_str())
+    }
+}
+
+#[cfg(feature = "json")]
+impl<'de> serde::Deserialize<'de> for TimeFrame {
+    fn deserialize<D: serde::Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s = String::deserialize(d)?;
+        TimeFrame::from_str(&s)
+            .ok_or_else(|| serde::de::Error::unknown_variant(&s, &[
+                "1m", "3m", "5m", "15m", "30m", "1h", "4h", "1d", "1w", "1M",
+            ]))
     }
 }
